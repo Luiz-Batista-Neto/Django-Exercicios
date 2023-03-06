fbd070751394051435cf553ca932a6a6ba21e941
Luiz Batista Neto <luizbneto45@hotmail.com>
2023-03-06 16:36:14 -0300

    Commit_Corrigindo
    Correção da atividade

--- Atividade2/usuarios/views.py
+++ Atividade2/usuarios/views.py
@@ -4,17 +4,17 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 usuarios = [
-    {"Id": 1, "Nome": "Travis", "Idade": 27},
-    {"Id": 2, "Nome": "Jeane", "Idade": 20},
-    {"Id": 3, "Nome": "Lucas", "Idade": 37}
+    {"id": 1, "nome": "Travis", "idade": 27},
+    {"id": 2, "nome": "Jeane", "idade": 20},
+    {"id": 3, "nome": "Lucas", "idade": 37}
 ]
 
 def listar_Usuarios(request):
     context = {
-        "Usuarios": usuarios
+        "usuarios": usuarios
     }
     return render(request, 'listar_usuarios.html', context)
 
-def detalhes_Usuarios(request):
+def detalhes_Usuarios(request, id):
     usuario = usuarios[id - 1]
     return render(request, 'detalhe_usuarios.html', {"usuario": usuario})
