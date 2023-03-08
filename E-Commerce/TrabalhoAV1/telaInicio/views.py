1be4f9788e5685ca914ccbfd39f5ce67eb959c7e
Luiz Batista Neto <luizbneto45@hotmail.com>
2023-03-08 00:19:22 -0300

    E-Commerce criado
    Trabalho para primeira avaliação de Django

---
+++ E-Commerce/TrabalhoAV1/telaInicio/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def gameHome(request):
+    return render(request, "home.html")
+
+
+games = [
+    {"id": 1, "Title": "jogo 1", "genre": "action"},
+    {"id": 2, "Title": "jogo 2", "genre": "rpg"},
+    {"id": 3, "Title": "jogo 3", "genre": "strategy"},
+]
+
+
+def listar_jogos(request):
+    context = {"games": games}
+    return render(request, "listar_jogo.html", context)
+
+
+def detalhe_jogo(request, id):
+    game = games[id - 1]
+    return render(request, "detalhe_jogo.html", {"game": game})
